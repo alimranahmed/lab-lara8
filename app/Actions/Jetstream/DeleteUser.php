fbb5e2ca208ec1d5c52b3bd0012afa557a2a6488
Al- Imran Ahmed <al.imran.cse@gmail.com>
2020-09-22 11:00:26 +0600

    Livewire installed through jetstream

---
+++ app/Actions/Jetstream/DeleteUser.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Actions\Jetstream;
+
+use App\Models\User;
+use Laravel\Jetstream\Contracts\DeletesUsers;
+
+class DeleteUser implements DeletesUsers
+{
+    /**
+     * Delete the given user.
+     *
+     * @param  mixed  $user
+     * @return void
+     */
+    public function delete($user)
+    {
+        $user->delete();
+    }
+}
